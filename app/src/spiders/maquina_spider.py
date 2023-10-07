a00ec8b5b3448a02be1c854da05a2757a7b89070
Stanislav <gavrilchenko.stanislav@gmail.com>
2023-10-07 07:12:52 +0100

    maquina base with one domain

--- app/src/spiders/maquina_spider.py
+++ app/src/spiders/maquina_spider.py
@@ -1,4 +1,5 @@
 import time
+import json
 from scrapy import Spider
 from scrapy.selector import Selector
 
@@ -14,35 +15,48 @@ from src.constants import LINKS, ENTRANCE_MODAL
 class MaquinaSpider(Spider):
     name = "maquina"
     allowed_domains = ["kuantokusta.pt"]
-    start_urls = LINKS
+    start_urls = [
+        f"https://www.kuantokusta.pt/search?q=cafe+maquina&pag={page}"
+          for page in range(1, 5)
+    ]
 
     def __init__(self):
         service = webdriver.ChromeService(executable_path="/usr/bin/chromedriver")
         self.driver = webdriver.Chrome(service=service)
 
     def remove_modal(self):
-        time.sleep(10)
+        time.sleep(5)
         accept_xpath = ENTRANCE_MODAL
         click_button(self.driver, accept_xpath)
         
     def parse(self, response):
-        self.driver.get(response.url)
-        self.remove_modal()
+        time.sleep(2)
 
-        items_grid = '//*[@id="searchForm"]/div/div[3]/div[3]'
-        data = Selector(response).xpath(items_grid)
+        if response.url == LINKS[0]:
+            self.driver.get(response.url)
+            self.remove_modal()
+     
+        grid = response.xpath('.//div[@class="product-item"]')
+        num = 1
+        for item in grid:
+            # here should be a separate controller with xpaths by domain
 
-        for item in data:
-            print(item)
+            print(num)
+            big_price = item.xpath('.//span[@class="price-interval"]/text()').get()
+            small_price = item.xpath('.//span[@class="small-price-interval"]/text()').get()
+            price = small_price or big_price
+            print(f"price: {price}")
+            title = item.xpath('.//h2[@itemprop="name"]/text()').get()
+            print(f"title: {title}")
+            link = item.xpath('.//div[@class="product-item-inner"]/a/@href').get()
+            print(f"link: {link}")
+            num += 1
 
-        # while True:
-        #     next = self.driver.find_element_by_xpath('//td[@class="pagn-next"]/a')
-
-        #     try:
-        #         next.click()
-
-        #         # get the data and write it to scrapy items
-        #     except:
-        #         break
-
-        self.driver.close()
+            # and a separate class to standartize the output, maye it's one class
+            yield {
+                "date": "2023-10-06",
+                "title": title,
+                "price": price,
+                "link": link,
+            }
+        
